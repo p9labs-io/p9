d7c41143c664291714878a6335acde7463ee8184
faridmmv <34021941+faridmmv@users.noreply.github.com>
2026-02-16 13:21:44 +0700

    Dns rdap (#3)
    * rdap lookup

--- internal/dns/rdap.go
+++ internal/dns/rdap.go
@@ -1,3 +1,12 @@
+/*
+ *
+ *  * Copyright 2026 P9 Labs
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *
+ */
+
 package dns
 
 import (
@@ -8,20 +17,87 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
+// Lookup
+
+func LookupDomain(domain string) {
+
+	baseURL := RdapServer(domain)
+	url := fmt.Sprint(baseURL, "domain/", domain)
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Failed to query RDAP server %s: %v", url, err)
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 399 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	type Event struct {
+		EventAction string `json:"eventAction"`
+		EventDate   string `json:"eventDate"`
+	}
+
+	type Nameservers struct {
+		LdhName string `json:"ldhName"`
+	}
+
+	type SecureDNS struct {
+		DelegationSigned bool `json:"delegationSigned"`
+		MaxSigLife       uint `json:"maxSigLife"`
+	}
+
+	type RdapResponse struct {
+		LdhName     string        `json:"ldhName"`
+		Nameservers []Nameservers `json:"nameservers"`
+		Status      []string      `json:"status"`
+		Events      []Event       `json:"events"`
+		SecureDNS   *SecureDNS    `json:"secureDNS"`
+	}
+
+	var rdapResponse RdapResponse
+
+	err = json.Unmarshal(body, &rdapResponse)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	output, err := json.MarshalIndent(rdapResponse, "", "  ") // 2-space indent
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	os.Stdout.Write(output)
+
+}
+
+// Get RDAP server url
 type RdapConfig struct {
 	Services [][][]string `json:"services"`
 }
 
 var rdapconfig RdapConfig
 
-//	//TODO: avoid repetitive file download
+func RdapServer(domain string) string {
+	config := createConfig()
+	tld := extractTLD(domain)
+	baseURL := config[tld]
+	if baseURL == "" {
+		log.Fatalf("TLD '.%s' is not supported by RDAP", tld)
+	}
+	return baseURL
+}
 
-func RdapServer(tld string) string {
-	configPath := createConfig()
-	//load map from config to var or read map from file?
+func extractTLD(domain string) string {
+	i := strings.LastIndex(domain, ".")
+	tld := domain[i+1:]
+	return tld
 }
 
 func getRdapConfig() {
@@ -70,16 +146,6 @@ func getRdapConfig() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Printf("Successfully created: %s\n", filePath)
-
-	//var rawconfig [][]string //got json from url https://data.iana.org/rdap/dns.json, .services field
-	//config := make(map[string]string)
-	//for _, v := range rawconfig {
-	//	config[v[0]] = v[1]
-	//}
-	//return config
-
 }
 
 // 1 domain as a key
@@ -105,29 +171,54 @@ func transformToMap(rawconfig RdapConfig) map[string]string {
 	return config
 }
 
-func createConfig() string {
-	// build file path
+func createConfig() map[string]string {
 	cfgdir, err := os.UserConfigDir()
 	if err != nil {
-		fmt.Printf("Error getting home directory: %v\n", err)
+		log.Fatalf("Error getting config directory: %v", err)
 	}
 
 	configDir := filepath.Join(cfgdir, ".p9")
 	filePath := filepath.Join(configDir, "rdap_config")
+
 	fileInfo, err := os.Stat(filePath)
+
+	if os.IsNotExist(err) {
+		getRdapConfig()
+		config, err := loadConfigFromFile(filePath)
+		if err != nil {
+			log.Fatalf("Can't read newly created config: %v", err)
+		}
+		return config
+	}
+
+	if err != nil {
+		log.Fatalf("Can't access file %s: %v", filePath, err)
+	}
+
 	age := time.Since(fileInfo.ModTime())
-	if os.IsNotExist(err) || age.Hours() > 720 {
+	if age.Hours() > 720 {
 		getRdapConfig()
 	}
 
-	return filePath
+	config, err := loadConfigFromFile(filePath)
+	if err != nil {
+		log.Fatalf("Can't read config file: %v", err)
+	}
+
+	return config
 }
 
 func loadConfigFromFile(filePath string) (map[string]string, error) {
-	// TODO:
-	// 1. Open file
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
 
-	// 2. Create json.Decoder
-	// 3. Decode into map[string]string
-	// 4. Return the map
+	var config map[string]string
+	err = json.Unmarshal(data, &config) // Convert bytes to map
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
 }
